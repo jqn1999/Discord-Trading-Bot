3f5570b83d4c942b54458ab5b5622375d5d045b0
Joshua Nguyen <jnguyensk@gmail.com>
2021-06-18 14:34:22 -0400

    Finished pulling options data from API

--- cogs/trading.py
+++ cogs/trading.py
@@ -31,5 +31,33 @@ class Trading(commands.Cog):
         except:
             await ctx.channel.send("That stock ticker does not exist!")
 
+    @commands.command()
+    async def getOption(self, ctx, stock, option, date):
+        #try:
+        stock = stock.upper()
+        strike = option[:-1]
+        optionType = option[-1]
+        month,day,year = date.split('/')
+
+        if optionType.lower() == 'c':
+            optionType = 'CALL'
+        elif optionType.lower() == 'p':
+            optionType = 'PUT'
+        else:
+            await ctx.channel.send("Format incorrect, please recheck.")
+            return
+
+        userRequest = requests.get(f"https://api.tdameritrade.com/v1/marketdata/chains?apikey={td_token}&symbol={stock}&contractType={optionType}&includeQuotes=FALSE&strike={strike}&fromDate=20{year}-{month}-{day}&toDate=20{year}-{month}-{day}", headers = headers)
+        strike = "{:.1f}".format(float(strike))
+        key = list(userRequest.json()[f"{optionType.lower()}ExpDateMap"].keys())[0]
+
+        mark = userRequest.json()[f"{optionType.lower()}ExpDateMap"][key][strike][0]["mark"]
+        mark = "{:.2f}".format(mark)
+
+        await ctx.channel.send(f"The current price of {stock} {strike} {optionType} {month}/{day}/{year} is ${mark}.")
+        print(f"{ctx.author} requested {stock} option information")
+        #except:
+            #await ctx.channel.send("Format incorrect, please recheck.")
+
 def setup(client):
     client.add_cog(Trading(client))
